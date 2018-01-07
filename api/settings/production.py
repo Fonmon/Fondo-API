3556cf524ae4410a575a1693fc2a7e01296a07e4
MinMiguelM <cmiguelmg@gmail.com>
2018-01-07 15:36:27 -0500

    prod env

--- api/settings/production.py
+++ api/settings/production.py
@@ -8,7 +8,7 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','fonmon.minagle.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
