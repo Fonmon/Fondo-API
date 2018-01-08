acb5a0c6a65fe97e5e9f850cf0f4892b9a182a1d
MinMiguelM <cmiguelmg@gmail.com>
2018-01-08 16:18:04 -0500

    cors settings

--- api/settings/production.py
+++ api/settings/production.py
@@ -13,29 +13,6 @@ ALLOWED_HOSTS = ['api-fonmon.minagle.com','127.0.0.1']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'fondo_api',
-    'rest_framework',
-    'rest_framework.authtoken',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
