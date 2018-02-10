6a4302cc84f71b3e7c0b915c606dcc46d9c74052
MinMiguelM <cmiguelmg@gmail.com>
2018-02-10 14:16:19 -0500

    docker context

--- container/build-docker.sh
+++ container/build-docker.sh
@@ -4,4 +4,6 @@ set -x
 # Validation is not added but an argument is
 # mandatory for running this script
 
-docker build -t fonapi_image:$1 container/
+docker build -t fonapi_image:$1 \
+	-f container/Dockerfile \
+	.
