2a90e0cd07eacc4ff5ce7ea19a7141ec3bd377b1
Miguel Montañez <cmiguelmg@gmail.com>
2018-03-26 10:34:41 -0500

    fix logging typo

--- api/settings/production.py
+++ api/settings/production.py
@@ -24,7 +24,7 @@ LOGGING = {
         'require_debug_false': {
             '()':'django.utils.log.RequireDebugFalse'
         }
-    }
+    },
     'handlers': {
         'console': {
             'level': 'ERROR',
