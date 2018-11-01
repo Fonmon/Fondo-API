e3e25b46ced9b7ed6dea59ebe901458709fc13a2
MinMiguelM <cmiguelmg@gmail.com>
2018-10-31 19:39:05 -0500

    test for date utils. #18

--- fondo_api/logic/date_utils.py
+++ fondo_api/logic/date_utils.py
@@ -11,50 +11,20 @@ def days360(startDate, endDate, europeanMethod = False):
         startDate = endDate
         endDate = tmpDate
 
-    startDate, endDate = normalizeDates(startDate, endDate, europeanMethod)
-    
-    startDay = 30 if startDate.month == 2 and startDate.day == calendar.monthrange(startDate.year, startDate.month)[1] else startDate.day
-    endDay = 30 if endDate.month == 2 and endDate.day == calendar.monthrange(endDate.year, endDate.month)[1] else endDate.day
+    startDay = startDate.day
+    endDay = endDate.day
 
-    diffYear = endDate.year - startDate.year
-    if (diffYear > 0) and ((endDate.month < startDate.month) or (endDate.month == startDate.month and endDay < startDay)):
-        diffYear -= 1
-
-    diffMonth = endDate.month - startDate.month
-    if diffMonth < 0:
-        diffMonth = 12 - abs(diffMonth)
-    if diffMonth > 0 and endDay < startDay:
-        diffMonth -= 1
-    elif diffMonth == 0 and endDate.year > startDate.year and endDay < startDay:
-        diffMonth = 11
-
-    diffDay = endDay - startDay
-    if diffDay < 0:
-        diffDay = 30 - abs(diffDay)
-    
-    if diffYear < 0 or diffMonth < 0 or diffDay < 0:
-        raise Exception('Something went wrong: dy{} dm{} dd{}'.format(diffYear, diffMonth, diffDay))
-
-    return (diffYear * 12 * 30) + (diffMonth * 30) + diffDay
-
-def normalizeDates(startDate, endDate, europeanMethod = False):
     if europeanMethod:
-        return (normalizeEuropeDate(startDate), normalizeEuropeDate(endDate))
-    
-    # NASD method
-    daysInMonthStart = calendar.monthrange(startDate.year, startDate.month)[1]
-    daysInMonthEnd = calendar.monthrange(endDate.year, endDate.month)[1]
-    if daysInMonthStart == startDate.day:
-        if startDate.month != 2:
-            startDate = startDate.replace(day=30)
-    if daysInMonthEnd == endDate.day:
-        if (startDate.day < 30 and startDate.month != 2) or (startDate.day < daysInMonthStart and startDate.month == 2):
-            endDate = endDate + datetime.timedelta(days = 1)
-        elif (startDate.day >= 30 and startDate.month != 2) or (startDate.day >= daysInMonthStart and startDate.month == 2):
-            endDate = endDate.replace(day=30)
-    return (startDate, endDate)
+        startDay = min(startDay, 30)
+        endDay = min(endDay, 30)
+    else:
+        # US NASD
+        if isLastDay(startDate):
+            startDay = 30
+        if startDay == 30 and endDay == 31:
+            endDay = 30
 
-def normalizeEuropeDate(date):
-    if date.day > 30:
-        return date.replace(day=30)
-    return date
+    return ((endDate.year - startDate.year)*360) + ((endDate.month - startDate.month)*30) + (endDay - startDay)
+
+def isLastDay(date):
+    return calendar.monthrange(date.year, date.month)[1] == date.day
--- fondo_api/tests/test_date_utils.py
+++ fondo_api/tests/test_date_utils.py
@@ -1,6 +1,32 @@
+import datetime
 from django.test import TestCase
 from ..logic.date_utils import days360
 
 class DateUtilsTest(TestCase):
-    def test_case_1(self):
-        pass
+    def test_case_nasd(self):
+        self.assertEqual(days360(datetime.date(2018,3,28), datetime.date(2019,3,27)), 359)
+        self.assertEqual(days360(datetime.date(2018,2,28), datetime.date(2018,3,28)), 28)
+        self.assertEqual(days360(datetime.date(2018,4,29), datetime.date(2018,5,31)), 32)
+        self.assertEqual(days360(datetime.date(2018,4,30), datetime.date(2018,5,31)), 30)
+        self.assertEqual(days360(datetime.date(2018,2,27), datetime.date(2018,5,31)), 94)
+        self.assertEqual(days360(datetime.date(2018,2,28), datetime.date(2018,3,31)), 30)
+        self.assertEqual(days360(datetime.date(2018,3,29), datetime.date(2018,5,31)), 62)
+        self.assertEqual(days360(datetime.date(2018,5,31), datetime.date(2018,3,29)), 62)
+        self.assertEqual(days360(datetime.date(2018,3,10), datetime.date(2018,3,31)), 21)
+        self.assertEqual(days360(datetime.date(2012,2,29), datetime.date(2013,2,28)), 358)
+        self.assertEqual(days360(datetime.date(2016,1,1), datetime.date(2016,12,31)), 360)
+        self.assertEqual(days360(datetime.date(2012,2,29), datetime.date(2016,2,29)), 1439)
+
+    def test_case_european(self):
+        self.assertEqual(days360(datetime.date(2018,3,28), datetime.date(2019,3,27), True), 359)
+        self.assertEqual(days360(datetime.date(2018,2,28), datetime.date(2018,3,28), True), 30)
+        self.assertEqual(days360(datetime.date(2018,4,29), datetime.date(2018,5,31), True), 31)
+        self.assertEqual(days360(datetime.date(2018,4,30), datetime.date(2018,5,31), True), 30)
+        self.assertEqual(days360(datetime.date(2018,2,27), datetime.date(2018,5,31), True), 93)
+        self.assertEqual(days360(datetime.date(2018,2,28), datetime.date(2018,3,31), True), 32)
+        self.assertEqual(days360(datetime.date(2018,3,29), datetime.date(2018,5,31), True), 61)
+        self.assertEqual(days360(datetime.date(2018,5,31), datetime.date(2018,3,29), True), 61)
+        self.assertEqual(days360(datetime.date(2018,3,10), datetime.date(2018,3,31), True), 20)
+        self.assertEqual(days360(datetime.date(2012,2,29), datetime.date(2013,2,28), True), 359)
+        self.assertEqual(days360(datetime.date(2016,1,1), datetime.date(2016,12,31), True), 359)
+        self.assertEqual(days360(datetime.date(2012,2,29), datetime.date(2016,2,29), True), 1440)
