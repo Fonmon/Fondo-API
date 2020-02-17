9f491c456ea4896a1fc0a54b551777ac06f5c890
MinMiguelM <cmiguelmg@gmail.com>
2020-02-16 19:22:10 -0500

    set env remove previous vars, so change option

--- scripts/trigger-deploy.sh
+++ scripts/trigger-deploy.sh
@@ -24,8 +24,8 @@ gcloud -q auth configure-docker
 gcloud -q container images delete us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --force-delete-tags
 docker push us.gcr.io/$PROJECT_ID/fonmon_api:$TAG
 
-gcloud -q run deploy $SERVICE --allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --set-env-vars=CURRENT_API_APP=api
-gcloud -q run deploy $SERVICE_SCHED --no-allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --set-env-vars=CURRENT_API_APP=scheduler
-gcloud -q run deploy $SERVICE_WORKER --no-allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --set-env-vars=CURRENT_API_APP=worker
+gcloud -q run deploy $SERVICE --allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --update-env-vars=CURRENT_API_APP=api
+gcloud -q run deploy $SERVICE_SCHED --no-allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --update-env-vars=CURRENT_API_APP=scheduler
+gcloud -q run deploy $SERVICE_WORKER --no-allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_api:$TAG --update-env-vars=CURRENT_API_APP=worker
 
 exit 0
