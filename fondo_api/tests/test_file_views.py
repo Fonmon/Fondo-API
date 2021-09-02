71fab35dcc8ac03029de903efd69541de1b10618
Miguel Montanez <cmiguelmg@gmail.com>
2021-09-02 17:57:45 -0500

    tests for mail and file finished, new version of application can be release now

--- fondo_api/tests/test_file_views.py
+++ fondo_api/tests/test_file_views.py
@@ -45,3 +45,82 @@ class FileViewTest(AbstractTest):
 			**self.get_auth_header(self.token)
 		)
     self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+  @patch.object(Client, 'get_bucket')
+  def test_post_file(self, bucket):
+    blob = MagicMock()
+    blob.exists.return_value = False
+    # blob.upload_from_file.side_effect = Exception('error uploading file')
+    bucket.return_value.blob.return_value = blob
+
+    self.assertEqual(len(File.objects.all()), 15)
+    response = self.client.post(
+      reverse(view_file),
+      data=encode_multipart('file', {
+        'name': 'New file',
+        'file': MagicMock(),
+        'type': 0
+      }),
+			content_type='multipart/form-data; boundary=file',
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    self.assertEqual(len(File.objects.all()), 16)
+
+  @patch.object(Client, 'get_bucket')
+  def test_post_file_already_in_db(self, bucket):
+    blob = MagicMock()
+    blob.exists.return_value = True
+    bucket.return_value.blob.return_value = blob
+
+    self.assertEqual(len(File.objects.all()), 15)
+    response = self.client.post(
+      reverse(view_file),
+      data=encode_multipart('file', {
+        'name': 'New file',
+        'file': MagicMock(),
+        'type': 0
+      }),
+			content_type='multipart/form-data; boundary=file',
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    self.assertEqual(len(File.objects.all()), 15)
+
+  @patch.object(Client, 'get_bucket')
+  def test_get_url_not_found(self, bucket):
+    response = self.client.get(
+      reverse(view_file_detail, kwargs={'id': 0}),
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+  @patch.object(Client, 'get_bucket')
+  def test_get_url(self, bucket):
+    file = File.objects.all()[0]
+
+    blob = MagicMock()
+    blob.generate_signed_url.return_value = "This is a URL"
+    bucket.return_value.blob.return_value = blob
+    response = self.client.get(
+      reverse(view_file_detail, kwargs={'id': file.id}),
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['url'], "This is a URL")
+
+  def test_get_files_by_type(self):
+    response = self.client.get(
+      "%s?type=0" % reverse(view_file),
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 15)
+
+  def test_get_files_not_type(self):
+    response = self.client.get(
+      reverse(view_file),
+			**self.get_auth_header(self.token)
+		)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 15)
