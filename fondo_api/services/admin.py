27e8551f6d29cfb91791740d31989dd16e6eb07e
Miguel Montañez <cmiguelmg@gmail.com>
2023-10-20 18:47:06 -0500

    send notifications to queue to be read by lambda function (#91)

--- fondo_api/services/admin.py
+++ fondo_api/services/admin.py
@@ -2,9 +2,16 @@ from fondo_api.enums import EmailTemplate
 
 class AdminService:
 
-  def __init__(self, mail_service = None):
+  def __init__(self, mail_service = None, notification_service = None):
     self.__mail_service = mail_service
+    self.__notification_service = notification_service
 
   def test_email(self, user):
-    print(user.email)
     self.__mail_service.send_mail(EmailTemplate.TEST, [user.email], None)
+
+  def test_notifications(self, user):
+    self.__notification_service.send_notification(
+      [user.id],
+      "Test Notification", 
+			"/"
+    )
