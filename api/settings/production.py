5640751f635d296c314f319db34640c54d6445a2
Miguel Montanez <cmiguelmg@gmail.com>
2026-05-26 17:09:03 -0500

    adding moodlabel to allowed apps cors

--- api/settings/production.py
+++ api/settings/production.py
@@ -10,7 +10,7 @@ ENVIRONMENT = 'production'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_DOMAIN'),'127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST_DOMAIN'),'127.0.0.1','.moodlabelmusic.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
